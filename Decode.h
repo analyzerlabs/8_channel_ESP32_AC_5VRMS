ceb82a80744c14d05c4d023e5b32d1625acdc5ae
MiguelQuispe <fisicomiguel@gmail.com>
2021-11-13 16:51:33 -0500

    Update Decode.h
    Co-Authored-By: OrlandoGironV <67022470+ORLANDOGIRONV@users.noreply.github.com>

--- Decode.h
+++ Decode.h
@@ -2,27 +2,41 @@ char *changeValues(char *p){
     char *r;
 
     if(&p[0] == "L" && &p[1]== "C"){
+        //MODO LC0
         if(&p[2]=="0"){
             for(int i=0;i<16;i=i+2){                   //evalua la trama para el modo LC0
                 if(&p[4+i] =="," && &p[5+i]>= "A" && &p[5+i]<="K" ){}
                 else {return r;} 
             }
-            for(int i=0;i<8;i++) r[i]= p[4];    //Guarda los valores del modo LC0
- 
+            for(int i=0;i<8;i++) r[i]= p[5+2*i];    //Guarda los valores del modo LC0
         }
+        //Modo LC1
         else if(&p[2]=="1"){
             if(&p[4] =="," && &p[5]>= "A" && &p[5]<="K" ){
-
+                for(int i=0;i<8;i++)r[i]= p[5];   //Guarda el valor recibido en todos los canales
+            } 
         }
-            
-    }
 
-    else{
-        return r;
-    }
+        //MODO 8LC2
+        else if(&p[2]=="2"){
+            if(&p[4] =="," && (&p[5]== "S" || &p[5]=="R") ){
+                if(&p[5]=="S"){
+                    for(int i=0;i<4;i++){ 
+                        r[i] = "K";  
+                    }
+                    for(int i=4;i<8;i++){
+                         r[i] = "A"; 
+                    }
+
+                }
+            }
+            else {
+                return r;
+            }
+        }
     return r;
+    }
 }
-
 void mode(int c){
 
 
