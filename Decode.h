b2e99fa683cf1f102d2ec17e7d0dc154c5872821
MiguelQuispe <fisicomiguel@gmail.com>
2021-11-13 14:47:24 -0500

    Update Decode.h
    Co-Authored-By: OrlandoGironV <67022470+ORLANDOGIRONV@users.noreply.github.com>

--- Decode.h
+++ Decode.h
@@ -9,8 +9,7 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint8_t txValue = 0;
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+
 
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
